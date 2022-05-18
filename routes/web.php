72607e1f00fd7177bba178ec9b4a4e47904126ee
Aya Osama <aya55osma2001@gmail.com>
2022-05-18 21:22:58 +0200

    modify database file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
@@ -13,6 +14,6 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('register');
 });
-Route::get('/contact_us', function() {
+Route::get('/contact_us', function () {
     return view('contact_us');
 });
