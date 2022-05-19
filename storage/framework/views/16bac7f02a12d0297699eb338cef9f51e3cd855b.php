9e65008b26ba2431574c80f01562d04802ff0285
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2022-05-19 11:12:22 +0200

    All Files of Laravel

---
+++ storage/framework/views/16bac7f02a12d0297699eb338cef9f51e3cd855b.php
@@ -0,0 +1,5 @@
+<!-- Badawy -->
+
+
+
+<?php echo $__env->make('main-template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH L:\HIS_Proj\resources\views/contact_us.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/3c1073ad113a1a22dbf740bec4ea1183648e9787.php
@@ -0,0 +1,5 @@
+<!-- Badawy -->
+
+
+
+<?php echo $__env->make('main-template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Badawy\Desktop\HIS_Project_Kovido\resources\views/contact_us.blade.php ENDPATH**/ ?>
