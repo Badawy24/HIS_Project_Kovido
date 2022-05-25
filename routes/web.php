d6334d4b4f9d87087914de4c7e28a7d2611835fc
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2022-05-25 14:12:55 +0200

    Contact Doctor

--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,29 @@
 use App\Http\Controllers\adminController;
 use App\Http\Controllers\ForgetPassController;
 use App\Http\Controllers\Login_Controller;
+use App\Http\Controllers\ContactDocController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 //use App\Http\Controllers\Request;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 /* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login', [Login_Controller::class , 'showLogin'])->middleware('logoutmiddle');
-Route::post('/login', [Login_Controller::class , 'login']);
+Route::get('/login', [Login_Controller::class, 'showLogin'])->middleware('logoutmiddle');
+Route::post('/login', [Login_Controller::class, 'login']);
 
 
 Route::get('/register', function () {
     return view('register');
 })->middleware('logoutmiddle');
 
-Route::post('/register', [RegisterController::class , 'registration'])->name('register');
+Route::post('/register', [RegisterController::class, 'registration'])->name('register');
 
-Route::get('/logout', [Login_Controller::class , 'logout']);
+Route::get('/logout', [Login_Controller::class, 'logout']);
 
 
 Route::view('contact_us', 'contact_us')->name('contact_us');
@@ -46,18 +48,20 @@ Route::get('/new_test', function () {
     return view('new_test');
 });
 Route::get('/contact_doc', function () {
-    return view('contact_doc');
+    $doc_name = DB::select('select * from doctor');
+    return view('contact_doc')->with('doc_names', $doc_name);
 });
+Route::post('/sendDoc', [ContactDocController::class, 'sendDoc'])->name('sendDoc.email');
 
-Route::get('/forgetSendMail', [ForgetPassController::class , 'showForgetSendMail']);
-Route::post('/sendmailForget', [ForgetPassController::class , 'sendMailForgetPass']);
+Route::get('/forgetSendMail', [ForgetPassController::class, 'showForgetSendMail']);
+Route::post('/sendmailForget', [ForgetPassController::class, 'sendMailForgetPass']);
 
-Route::get('/resetPass', [ForgetPassController::class , 'showResetPass']);
-Route::post('/resetPass', [ForgetPassController::class , 'resetPassword']);
+Route::get('/resetPass', [ForgetPassController::class, 'showResetPass']);
+Route::post('/resetPass', [ForgetPassController::class, 'resetPassword']);
 
 
-Route::get('/admin_doc_data', [adminController::class , 'admin_doc_data'])->middleware('loginmiddle');
-Route::get('/admin_doc_msg', [adminController::class , 'Show_admin_doc_msg'])->middleware('loginmiddle');
-Route::post('/admin_doc_msg', [adminController::class , 'admin_doc_msg'])->middleware('loginmiddle');
-Route::get('/admin_dose_data', [adminController::class , 'admin_dose_data'])->middleware('loginmiddle');
-Route::get('/admin_test_data', [adminController::class , 'admin_test_data'])->middleware('loginmiddle');
+Route::get('/admin_doc_data', [adminController::class, 'admin_doc_data'])->middleware('loginmiddle');
+Route::get('/admin_doc_msg', [adminController::class, 'Show_admin_doc_msg'])->middleware('loginmiddle');
+Route::post('/admin_doc_msg', [adminController::class, 'admin_doc_msg'])->middleware('loginmiddle');
+Route::get('/admin_dose_data', [adminController::class, 'admin_dose_data'])->middleware('loginmiddle');
+Route::get('/admin_test_data', [adminController::class, 'admin_test_data'])->middleware('loginmiddle');
