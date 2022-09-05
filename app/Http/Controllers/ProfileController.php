2b6d854fc1de5e07b36ee7195345de690709f911
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2022-09-05 05:29:45 +0200

    Edit Calculate Age In Edit Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,9 +30,10 @@ class ProfileController extends Controller
             'pat_phone' => 'required|min:11|max:11',
             'birthday' => 'required',
         ]);
+        $age = Carbon::parse($request->birthday)->diff(Carbon::now())->y;
 
         $pat_id = session('user_id');
-        DB::update('update patient set pat_email = ?, pat_address =? , pat_phone =? , pat_DOF = ? where pat_id = ?', [$request->pat_email, $request->pat_address, $request->pat_phone, $request->birthday, $pat_id]);
+        DB::update('update patient set pat_email = ?, pat_address =? , pat_phone =? , pat_DOF = ? , pat_age = ? where pat_id = ?', [$request->pat_email, $request->pat_address, $request->pat_phone, $request->birthday, $age, $pat_id]);
         $patient = DB::select('select * from patient where pat_id = ?', [$pat_id]);
         return redirect('profile')->with(['patients' => $patient, 'success' => 'Your Data Updated']);
     }
---
+++ storage/framework/views/3aa162e7af04a92cb055e45c543321eab2d0b972.php
@@ -0,0 +1,184 @@
+
+<?php $__env->startSection('content'); ?>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="register-form">
+                        <!-- ////////////// -->
+                        <h2 class="head-cards h1 py-5 text-center">Welcome <span class="user-name">
+                                <?php $__currentLoopData = $patients; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $patient): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <?php echo e($patient->pat_fname); ?>
+
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </span> Can Edit Profile
+                        </h2>
+                        <?php if(Session::has('error')): ?>
+                            <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                <div>
+                                    <i class="fa-solid fa-triangle-exclamation"></i>
+                                    &nbsp;
+                                    <?php echo e(Session::get('error')); ?>
+
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                        <?php if(Session::has('success')): ?>
+                            <div class="alert alert-success d-flex align-items-center" role="alert">
+                                <i class="fa-regular fa-circle-check"></i>
+                                &nbsp;
+                                <div>
+                                    <?php echo e(Session::get('success')); ?>
+
+                                </div>
+                            </div>
+                        <?php endif; ?>
+                        <form action="/updateprofile" method="POST" class="row">
+                            <?php echo csrf_field(); ?>
+                            <?php $__currentLoopData = $patients; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $patient): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <div class="col-md-6">
+                                    <label for="f_name" class="form-label profile_label_style">FirstName</label>
+                                    <input class=" form-control input_profile" disabled type="text"
+                                        value="<?php echo e($patient->pat_fname); ?>" name="pat_fname" id="f_name"
+                                        aria-label="default
+                                                input example">
+
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="l_name"
+                                        class="form-label
+                                            profile_label_style">Last
+                                        Name</label>
+                                    <input
+                                        class="form-control col-auto
+                                            input_profile"
+                                        type="text" disabled value="<?php echo e($patient->pat_lname); ?>" name="pat_lname"
+                                        id="l_name" aria-label="default input example">
+                                </div>
+                                <div class="col-lg-12">
+                                    <label for="ssn"
+                                        class="form-label
+                                            profile_label_style">SSN</label>
+
+                                    <input class=" form-control
+                                            input_profile"
+                                        type="text" value="<?php echo e($patient->pat_SSN); ?>" name="pat_SSN" id="ssn"
+                                        disabled aria-label="default input example">
+                                </div>
+                                <div class="col-lg-12">
+                                    <label for="email"
+                                        class="form-label
+                                            profile_label_style">Email</label>
+                                    <input class=" form-control edit_label" type="email" value="<?php echo e($patient->pat_email); ?>"
+                                        name="pat_email" id="email" aria-label="default input example">
+                                    <?php $__errorArgs = ['pat_email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                        <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                            <i class="fa-solid fa-triangle-exclamation"></i>
+                                            <div><?php echo e($message); ?> </div>
+                                        </div>
+                                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                </div>
+
+                                <div class="col-lg-12">
+                                    <label for="address"
+                                        class="form-label
+                                            profile_label_style">Address</label>
+                                    <input class=" form-control" type="text" value="<?php echo e($patient->pat_address); ?>"
+                                        name="pat_address" id="address"
+                                        aria-label="default
+                                            input example">
+                                    <?php $__errorArgs = ['pat_address'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                        <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                            <i class="fa-solid fa-triangle-exclamation"></i>
+                                            <div><?php echo e($message); ?> </div>
+                                        </div>
+                                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                </div>
+                                <div class="col-lg-12">
+                                    <label for="phone"
+                                        class="form-label
+                                            profile_label_style">Phone</label>
+                                    <input class=" form-control" type="tel" value="<?php echo e($patient->pat_phone); ?>"
+                                        name="pat_phone" id="phone"
+                                        aria-label="default input
+                                            example">
+                                    <?php $__errorArgs = ['pat_phone'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                        <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                            <i class="fa-solid fa-triangle-exclamation"></i>
+                                            <div><?php echo e($message); ?> </div>
+                                        </div>
+                                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                </div>
+                                <div class="col-lg-8">
+                                    <label for="birthday"
+                                        class="form-label
+                                profile_label_style">Birthday</label>
+                                    <input class=" form-control" type="date" value="<?php echo e($patient->pat_DOF); ?>"
+                                        name="birthday" id="birthday"
+                                        aria-label="default input
+                                example">
+                                    <?php $__errorArgs = ['birthday'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                                        <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                            <i class="fa-solid fa-triangle-exclamation"></i>
+                                            <div><?php echo e($message); ?> </div>
+                                        </div>
+                                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                                </div>
+                                <div class="col-lg-4">
+                                    <label for="age"
+                                        class="form-label
+                                            profile_label_style">Age</label>
+                                    <input disabled class=" form-control input_profile" type="text"
+                                        value="<?php echo e($patient->pat_age); ?>" name="age" id="age"
+                                        aria-label="default input
+                                            example">
+                                </div>
+                                <input type="submit"
+                                    class="btn btn-primary
+                                        mb-3 submit"
+                                    value="Save" name="register-user">
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6 center-image">
+                    <div class="login-img ">
+                        <img id="editprof" src="/images/editprof.png" class="img-fluid" width="200px"
+                            height="200px" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('main-template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Badawy\Desktop\v0.1\HIS_Project_Kovido\resources\views/Editprofile.blade.php ENDPATH**/ ?>
