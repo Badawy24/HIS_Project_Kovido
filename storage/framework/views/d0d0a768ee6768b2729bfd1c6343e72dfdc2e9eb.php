af35a1ed5fd277c2bb4f631eefce91de6907afbc
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2022-09-11 01:15:03 +0200

    Test Page Validation

--- storage/framework/views/d0d0a768ee6768b2729bfd1c6343e72dfdc2e9eb.php
+++ storage/framework/views/d0d0a768ee6768b2729bfd1c6343e72dfdc2e9eb.php
@@ -1,24 +1,23 @@
-@extends('main-template')
-@section('content')
+<?php $__env->startSection('content'); ?>
     <div class="main-div-login">
         <div class="container">
             <form action="update_data" method="POST" class="register-form test-form ">
-                @csrf
-                <input type="hidden" name="res_id" value="{{ $res_id }}">
+                <?php echo csrf_field(); ?>
+                <input type="hidden" name="res_id" value="<?php echo e($res_id); ?>">
                 <section class="row">
                     <div class="col-md-6">
 
                         <div class="col-md-12">
                             <label class="card-text" style="margin-left : 300px">Test</label>
                             <input class="inp-form form-control" style="text-align: center;" disabled type="text"
-                                value="{{ $my_test }}" name="test_name" id="test_name"
+                                value="<?php echo e($my_test); ?>" name="test_name" id="test_name"
                                 aria-label="default input example">
                         </div>
 
                         <div class="col-md-12">
                             <label class="card-text" style="margin-left : 250px">Health Care Center</label>
                             <input class="inp-form form-control" style="text-align: center;" disabled type="text"
-                                value="{{ $my_hcc }}" name="health_cc" id="health_cc"
+                                value="<?php echo e($my_hcc); ?>" name="health_cc" id="health_cc"
                                 aria-label="default input example">
                         </div>
 
@@ -26,14 +25,14 @@
 
                         <div class="col-md-12">
                             <label class="card-text">Date</label>
-                            <input class="inp-form form-control" type="date" value="{{ $my_date }}"
+                            <input class="inp-form form-control" type="date" value="<?php echo e($my_date); ?>"
                                 placeholder="test_date" name="test_date" id="test_date" aria-label="default input example"
                                 min="<?php echo date('Y-m-d'); ?>">
                         </div>
 
                         <div class="col-md-12">
                             <label class="card-text">Time</label>
-                            <input class="form-control" type="time" value="{{ $my_time }}" name="test_time"
+                            <input class="form-control" type="time" value="<?php echo e($my_time); ?>" name="test_time"
                                 placeholder="test_time" id="test_time" aria-label="default input example">
                         </div>
 
@@ -53,4 +52,6 @@
             </form>
         </div>
     </div>
-@endsection
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('main-template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Badawy\Desktop\HIS_Project_Kovido\resources\views/update_test.blade.php ENDPATH**/ ?>
