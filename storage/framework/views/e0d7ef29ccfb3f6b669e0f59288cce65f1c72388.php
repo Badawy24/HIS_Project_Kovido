4f94f363fa51b1804a2fcf6bf98a69e77c7c9773
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2022-09-14 22:32:32 +0200

    Add New Document

--- storage/framework/views/e0d7ef29ccfb3f6b669e0f59288cce65f1c72388.php
+++ storage/framework/views/e0d7ef29ccfb3f6b669e0f59288cce65f1c72388.php
@@ -3,51 +3,59 @@ use Illuminate\Support\Facades\DB;
 ?>
 
 <?php $__env->startSection('content'); ?>
-<div class="main-data">
-    <div class="row">
-        <div class="col-md-3">
-            <div class="data position-relative">
-                <h3>Patients <br /><span class="ms-2"><?php $pat = DB::select('select * from patient'); echo count($pat); ?></span></h3>
-                <i class="fa-solid fa-users me-3"></i>
+    <div class="main-data">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="data position-relative">
+                    <h3>Patients <br /><span class="ms-2"><?php $pat = DB::select('select * from patient');
+                    echo count($pat); ?></span></h3>
+                    <i class="fa-solid fa-users me-3"></i>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="data position-relative">
-                <h3>Doctors <br /><span class="ms-2"><?php $doc = DB::select('select * from doctor'); echo count($doc); ?></span></h3>
-                <i class="fa-solid fa-user-doctor"></i>
+            <div class="col-md-3">
+                <div class="data position-relative">
+                    <h3>Doctors <br /><span class="ms-2"><?php $doc = DB::select('select * from doctor');
+                    echo count($doc); ?></span></h3>
+                    <i class="fa-solid fa-user-doctor"></i>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="data position-relative">
-                <h3>Dose <br /><span class="ms-2"><?php $dose = DB::select('select * from dose'); echo count($dose); ?></span></h3>
-                <i class="fa-solid fa-square-virus"></i>
+            <div class="col-md-3">
+                <div class="data position-relative">
+                    <h3>Dose <br /><span class="ms-2"><?php $dose = DB::select('select * from dose');
+                    echo count($dose); ?></span></h3>
+                    <i class="fa-solid fa-square-virus"></i>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="data position-relative">
-                <h3>Test <br /><span class="ms-2"><?php $test = DB::select('select * from test'); echo count($test); ?></span></h3>
-                <i class="fa-solid fa-vials"></i>
+            <div class="col-md-3">
+                <div class="data position-relative">
+                    <h3>Test <br /><span class="ms-2"><?php $test = DB::select('select * from test');
+                    echo count($test); ?></span></h3>
+                    <i class="fa-solid fa-vials"></i>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="data position-relative">
-                <h3>Messages <br /><span class="ms-2"><?php $msg = DB::select('select * from doc_pat'); echo count($msg); ?></span></h3>
-                <i class="fa-solid fa-comment"></i>
+            <div class="col-md-3">
+                <div class="data position-relative">
+                    <h3>Messages <br /><span class="ms-2"><?php $msg = DB::select('select * from doc_pat');
+                    echo count($msg); ?></span></h3>
+                    <i class="fa-solid fa-comment"></i>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="data position-relative">
-                <h3>Consultation <br /><span class="ms-2"><?php $con = DB::select('select * from pat_consultation'); echo count($con); ?></span></h3>
-                <i class="fa-solid fa-forward-fast"></i>
+            <div class="col-md-3">
+                <div class="data position-relative">
+                    <h3>Consultation <br /><span class="ms-2"><?php $con = DB::select('select * from pat_consultation');
+                    echo count($con); ?></span></h3>
+                    <i class="fa-solid fa-forward-fast"></i>
+                </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="data position-relative">
-                <h3>Meeting <br /><span class="ms-2"><?php $meet = DB::select('select * from Meeting'); echo count($meet); ?></span></h3>
-                <i class="fa-sharp fa-solid fa-video"></i>
+            <div class="col-md-3">
+                <div class="data position-relative">
+                    <h3>Meeting <br /><span class="ms-2"><?php $meet = DB::select('select * from Meeting');
+                    echo count($meet); ?></span></h3>
+                    <i class="fa-sharp fa-solid fa-video"></i>
+                </div>
             </div>
         </div>
     </div>
-</div>
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('admin.admin-dashbord-temp', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Badawy\Desktop\v0.2\HIS_Project_Kovido\resources\views/admin/admin-dashbord.blade.php ENDPATH**/ ?>
